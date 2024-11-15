b22c1c774d72eb3490a26f6443f6eb831b42b0c1
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-11-15 13:04:23 -0300

    Criação das rotas e dos componentes de cardápio e pedido

--- ProjetoBurguerMania/src/app/app.routes.ts
+++ ProjetoBurguerMania/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PedidoComponent } from './pages/pedido/pedido.component';
+import { CardapioComponent } from './pages/cardapio/cardapio.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/pedido', pathMatch: 'full' },
+    { path: 'pedido', component: PedidoComponent },
+    { path: 'cardapio', component: CardapioComponent },
+    { path: 'cardapio/:categoria', component: CardapioComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ ProjetoBurguerMania/src/app/pages/cardapio/cardapio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio',
+  standalone: true,
+  imports: [],
+  templateUrl: './cardapio.component.html',
+  styleUrl: './cardapio.component.css'
+})
+export class CardapioComponent {
+
+}
