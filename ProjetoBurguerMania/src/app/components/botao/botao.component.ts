fa624c6befc29556f26db8a64038e0657a4a9edc
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-11-18 18:00:54 -0300

    Feita a estilização da página inicial

--- ProjetoBurguerMania/src/app/components/botao/botao.component.ts
+++ ProjetoBurguerMania/src/app/components/botao/botao.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-botao',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './botao.component.html',
   styleUrl: './botao.component.css'
 })
-export class BotaoComponent {
 
+//Classe com propriedades que podem ser modificadas dinamicamente
+export class BotaoComponent {
+  @Input() texto: string = 'Botão'; // Texto padrão
+  @Input() tamanhoFonte: string = '1';
+  @Input() valor: string = 'Teste';
+  @Input() cor: string = 'blue'; // Cor padrão
+  @Input() corFonte: string = 'black';
+  @Input() largura: string = '18';
 }
