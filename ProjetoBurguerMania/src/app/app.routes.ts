f63feddd38118db8fc7bbcd9c50450b1eff3394c
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-11-18 09:20:27 -0300

    Construidas as rotas entre as páginas

--- ProjetoBurguerMania/src/app/app.routes.ts
+++ ProjetoBurguerMania/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { PedidoComponent } from './pages/pedido/pedido.component';
 import { CardapioComponent } from './pages/cardapio/cardapio.component';
 
-const routes: Routes = [
-    { path: '', redirectTo: '/pedido', pathMatch: 'full' },
-    { path: 'pedido', component: PedidoComponent },
-    { path: 'cardapio', component: CardapioComponent },
-    { path: 'cardapio/:categoria', component: CardapioComponent }
+//Definição das rotas da URL para cada página
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, //Rota vazia para a home 
+    { path: 'pedido', component: PedidoComponent }, //Rota "/pedido" para a pagina de pedido
+    { path: 'cardapio', component: CardapioComponent }, //Rota "/cardapio" para a pagina do cardápio
+    { path: 'cardapio/:categoria', component: CardapioComponent } //Rota "/cardapio/categoria" para mostrar a página da categoria do produto
 ];
 
 @NgModule({
