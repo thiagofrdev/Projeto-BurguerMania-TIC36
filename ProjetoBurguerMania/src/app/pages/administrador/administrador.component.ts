6cab4d644a624e33f66ac1b29878d41b8a843079
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-12-03 12:00:09 -0300

    Criado o desigin inicial da página de Administrador

--- ProjetoBurguerMania/src/app/pages/administrador/administrador.component.ts
+++ ProjetoBurguerMania/src/app/pages/administrador/administrador.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BotaoComponent } from "../../components/botao/botao.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-administrador',
   standalone: true,
-  imports: [],
+  imports: [BotaoComponent, CardComponent],
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.css'
 })
